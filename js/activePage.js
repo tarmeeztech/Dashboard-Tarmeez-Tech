121cb0c4771c70cfe17128255a2ced23466cf509
aboodev404 <aboodev404@gmail.com>
2023-01-25 20:06:31 +0200

    Table optimization

--- js/activePage.js
+++ js/activePage.js
@@ -1,11 +1,14 @@
 const activePage = window.location.pathname;
-
+console.log(activePage)
 const navLinks = document.querySelectorAll(".sidebar a").
 forEach(link => {
     link.classList.remove('active');
+    if(activePage === "/") {
+        link.classList.add('active');
+        throw BreakException;
+        // link[0].classList.add('active');
+    }
     if(link.href.includes(`${activePage}`)) {
-        console.log(activePage);
-        console.log(link.href);
         // link.classList.toggle('active');
         link.classList.add('active');
     }
